8f85c9d79fbf0797616f6753dd9464e8fd1cf7d8
Telojuri <uribcn1994@gmail.com>
2025-02-13 14:37:09 +0100

    FEATURE: Cards flipping

--- Assets/CardController.cs
+++ Assets/CardController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -20,6 +21,12 @@ public class CardController : MonoBehaviour
     private CanvasGroup canvasGroup;
 
     public CombatantController combatantOwner;
+
+    public float flipDuration = 0.5f;
+    public float scaleFactor = 1.1f;
+    public GameObject cardFront;
+    public GameObject cardBack;
+    private bool flipingCard = false;
 
     private void Awake()
     {
@@ -58,7 +65,33 @@ public class CardController : MonoBehaviour
 
     public void TurnCard()
     {
+        if (flipingCard) return;
 
+        Sequence flipSequence = DOTween.Sequence();
+        flipingCard = true;
+        float targetRotation = isFaceDown ? 0 : 180;
+        // First half: Rotate to 90° and scale up together
+        flipSequence.Append(transform.DOScale(scaleFactor, flipDuration / 2).SetEase(Ease.OutQuad))
+                    .Join(transform.DORotate(new Vector3(0, 90, 0), flipDuration / 2, RotateMode.Fast).SetEase(Ease.InOutQuad))
+                    .OnStepComplete(() =>
+                    {
+                        Debug.Log("Flipeandoo");
+                        // Swap front and back visuals
+                        isFaceDown = !isFaceDown;
+                        cardFront.SetActive(!isFaceDown);
+                        cardBack.SetActive(isFaceDown);
+                        flipSequence.Append(transform.DOScale(1f, flipDuration / 2).SetEase(Ease.OutQuad))
+                       .Join(transform.DORotate(new Vector3(0, targetRotation, 0), flipDuration / 2, RotateMode.Fast).SetEase(Ease.InOutQuad))
+                       .OnComplete(() =>
+                       {
+                           flipingCard = false;
+                       });
+                    });
+        
+        // Second half: Rotate to 180° and scale back together
+        //flipSequence.Append(transform.DOScale(1f, flipDuration / 2).SetEase(Ease.OutQuad))
+                        //.Join(transform.DORotate(new Vector3(0, 180, 0), flipDuration / 2, RotateMode.Fast).SetEase(Ease.InOutQuad));
+        
     }
 
     public void CardPlayed(PlayableSocket socketToPlay)
